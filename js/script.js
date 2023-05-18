925c065a146bcf42c8ac9aa071e5ba8a710adb8f
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-05-18 19:04:18 +0200

    [FEAT]aggiunta stampa in pagina biglietto

--- js/script.js
+++ js/script.js
@@ -4,17 +4,22 @@ let kmValue = km.value
 let prezzo= kmValue * 0.21
 let anni= document.getElementById ('anni').value
 let sconto
-console.log(anni)
-console.log(prezzo)
-console.log(kmValue)
+let nome= document.getElementById ('nome').value
+
+
+let biglietto = document.getElementById('biglietto')
+
 
 
 if (button) {
     button.addEventListener('click', function() {
+        
         console.log('click')
         if (anni < 18 ){
             sconto = prezzo * 0.8
              console.log(sconto)
+            biglietto.innerHTML = `Biglietto ridotto. Costo del biglietto ${sconto.toFixed(2)} €`
+            console.log(biglietto)
         } 
     }); 
 }
@@ -23,6 +28,7 @@ if (button) {
     button.addEventListener('click', function() {
         if (anni > 65){
             let sconto_2 = prezzo * 0.6
+            biglietto.innerHTML = `Biglietto ridotto. Costo del biglietto ${sconto_2.toFixed(2)} €`
             console.log (sconto_2)
         }
     }); 
@@ -30,12 +36,14 @@ if (button) {
 
 if (button) {
     button.addEventListener('click', function() {
-        if ((anni > 17)  || (anni < 66 ))  {
-            let sconto = prezzo 
-            console.log (sconto)
+        if ((anni > 17) && (anni < 66 ))  {
+             prezzo 
+             biglietto.innerHTML = `Biglietto standard. Costo del biglietto ${prezzo.toFixed(2)} €`
+            console.log (prezzo)
 
         }
     }); 
 }
+
 
 
