71dcc84d2dc8727b890ba874acaa7a8709724036
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-05-18 17:05:26 +0200

    [Feat] inizio lavoro logica js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+let button = document.getElementById ('price_button')
+let km = document.getElementById ('km')
+let kmValue = km.value
+let prezzo= km * 0.21
+
+
+console.log(kmValue)
+console.log(eta)
+
+if (button) {
+    button.addEventListener('click', function() {
+       let eta= document.getElementById(eta).value = mylist.options[mylist.selectedIndex].text
+        console.log('click')
+        console.log(eta)
+        
+
+    }
+     ); 
+
+
+}
