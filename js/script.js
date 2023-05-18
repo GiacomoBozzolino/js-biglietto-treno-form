e53e63d0058d42afa2c524467b2f1a7fe9f0b226
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-05-18 20:23:10 +0200

    [FEAT]modificata stampa in pagina

--- js/script.js
+++ js/script.js
@@ -1,24 +1,19 @@
 let button = document.getElementById ('price_button')
+let clear = document.getElementById ('cancella')
 let km = document.getElementById ('km')
 let kmValue = km.value
 let prezzo= kmValue * 0.21
 let anni= document.getElementById ('anni').value
 let sconto
 let nome= document.getElementById ('nome').value
-
-
 let biglietto = document.getElementById('biglietto')
-
-
 
 if (button) {
     button.addEventListener('click', function() {
-        
-        console.log('click')
         if (anni < 18 ){
             sconto = prezzo * 0.8
              console.log(sconto)
-            biglietto.innerHTML = `Biglietto ridotto. Costo del biglietto ${sconto.toFixed(2)} €`
+            biglietto.innerHTML = `Ciao ${nome} hai diritto ad un biglietto ridotto. Costo del biglietto ${sconto.toFixed(2)} €`
             console.log(biglietto)
         } 
     }); 
@@ -28,7 +23,7 @@ if (button) {
     button.addEventListener('click', function() {
         if (anni > 65){
             let sconto_2 = prezzo * 0.6
-            biglietto.innerHTML = `Biglietto ridotto. Costo del biglietto ${sconto_2.toFixed(2)} €`
+            biglietto.innerHTML = `Ciao ${nome} hai diritto ad un biglietto ridotto. Costo del biglietto ${sconto_2.toFixed(2)} €`
             console.log (sconto_2)
         }
     }); 
@@ -38,7 +33,7 @@ if (button) {
     button.addEventListener('click', function() {
         if ((anni > 17) && (anni < 66 ))  {
              prezzo 
-             biglietto.innerHTML = `Biglietto standard. Costo del biglietto ${prezzo.toFixed(2)} €`
+             biglietto.innerHTML = `Ciao ${nome} hai diritto ad un biglietto standard. Costo del biglietto ${prezzo.toFixed(2)} €`
             console.log (prezzo)
 
         }
