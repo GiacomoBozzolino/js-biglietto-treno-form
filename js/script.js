a27187346f67c9fa1c2dc01bf0942f22023e9509
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-05-19 13:11:40 +0200

    [FEAT]aggiunta stampa dati biglietto in tabella

--- js/script.js
+++ js/script.js
@@ -13,29 +13,38 @@ if (button) {
     button.addEventListener('click', function() {
         // variabili con value
         km = document.getElementById ('km').value;
-        anni= document.getElementById ('anni').value;
+        anni= parseInt(document.getElementById ('anni').value);
         nome= document.getElementById ('nome').value;
-        prezzo= km * 0.21;
+        let prezzo= km * 0.21;
+        let sconto
+        let discount
         // sconto minorenni
         if (anni < 18 ){
             sconto = prezzo * 0.8;
+            discount= '20%'
             console.log(sconto);
             biglietto.innerHTML = `Ciao ${nome} hai diritto ad uno sconto del 20%. Costo del biglietto ${sconto.toFixed(2)} €`;
             console.log(biglietto);
         }
     //  sconto pensionati
         else if (anni > 65) {
-            let sconto_2 = prezzo * 0.6;
-            biglietto.innerHTML = `Ciao ${nome} hai diritto ad uno sconto del 40%. Costo del biglietto ${sconto_2.toFixed(2)} €`;
-            console.log (sconto_2);
+            sconto = prezzo * 0.6;
+            discount = '40%'
+            biglietto.innerHTML = `Ciao ${nome} hai diritto ad uno sconto del 40%. Costo del biglietto ${sconto.toFixed(2)} €`;
+            console.log(discount)
         }
 
 //    prezzo pieno
         else{   
-            prezzo 
-            biglietto.innerHTML = `Ciao ${nome} hai diritto ad un biglietto standard. Costo del biglietto ${prezzo.toFixed(2)} €`;
+            discount= '/'
+            sconto = prezzo 
+            biglietto.innerHTML = `Ciao ${nome} hai diritto ad un biglietto standard. Costo del biglietto ${sconto.toFixed(2)} €`;
             console.log (prezzo);   
-        }    
+        } 
+        document.getElementById('show-name').innerText = nome;
+        document.getElementById('show-km').innerText = km;
+        document.getElementById('show-discount').innerText = discount;
+        document.getElementById('show-price').innerText = sconto.toFixed(2) + '€';
     }); 
 }
 
@@ -46,6 +55,9 @@ if (cancella) {
         let anni= document.getElementById ('anni').value="";
         let nome= document.getElementById ('nome').value="";
         biglietto.innerHTML= "";
+        document.getElementById('show-name').innerText = "";
+        document.getElementById('show-km').innerText = "";
+        document.getElementById('show-discount').innerText = "";
+        document.getElementById('show-price').innerText = "";
     }); 
-
 }
